56e85b317e05d4093bb58f16e67221d302be6b80
rufunt <jazzoo@mail.ru>
2018-03-20 20:28:52 +0000

    Update aplication.html for haml

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -9,6 +9,12 @@ class PicsController < ApplicationController
      
     def create
       @pic = Pic.new(pic_params)
+      
+      if @pic.save
+        redirect_to @pic, notice: "It was posted!"
+      else
+        render 'new'
+      end
     end
     
     private
